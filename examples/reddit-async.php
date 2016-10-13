4e0b80236c77fa2c20fed33412acdfb5f78a420e
David Dan <davidwdan@gmail.com>
2016-10-12 20:34:08 -0400

    Make async example more RXy

--- examples/reddit-async.php
+++ examples/reddit-async.php
@@ -1,6 +1,7 @@
 <?php
 
 use React\EventLoop\Factory;
+use Rx\Observable;
 use Rx\Observer\CallbackObserver;
 use ApiClients\Pusher\AsyncClient;
 
@@ -15,13 +16,16 @@ $loop = Factory::create();
 $client = new AsyncClient($loop, require 'reddit.key.php');
 
 $subReddits = $argv;
-array_shift($subReddits);
-foreach ($subReddits as $subReddit) {
-    $client->channel($subReddit)->subscribe(new CallbackObserver(function ($event) {
+
+Observable::fromArray($subReddits)
+    ->skip(1)
+    ->flatMap(function ($subReddit) use ($client) {
+        return $client->channel($subReddit);
+    })
+    ->subscribe(new CallbackObserver(function ($event) {
         echo 'Channel: ', $event->channel, PHP_EOL;
         echo 'Event: ', $event->event, PHP_EOL;
         echo 'Data: ', $event->data, PHP_EOL;
     }));
-}
 
 $loop->run();
